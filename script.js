94e2a0753c5393ecbe6962d7e162dc4310254a15
amityadav <mjamit1947@gmail.com>
2023-05-25 13:37:39 +0530

    second change

--- script.js
+++ script.js
@@ -23,3 +23,26 @@ darkmode.onclick=()=>{
 
     }
 }
+function sendEmail(){
+    Email.send({
+        Host : "smtp.elasticemail.com",
+        Username : "username",
+        Password : "password",
+        To : 'them@website.com',
+        From : "you@isp.com",
+        Subject : "This is the subject",
+        Body : "And this is the body"
+    }).then(
+      message => alert(message)
+    );
+}
+let openBtn=document.querySelector("#open-btn");
+openBtn.addEventListener("click",function(){
+    document.querySelector(".popup").classList.add("active");
+});
+//console.log(openBtn);
+let closeBtn=document.querySelector(".popup .close-btn");
+closeBtn.addEventListener("click",function(){
+    document.querySelector(".popup").classList.remove("active");
+});
+console.log(closeBtn);
